b2b4baedc99350dcaebd579a5fcc3a45718345a4
fikys <maximfeok2200@gmail.com>
2026-08-17 23:53:58 +0300

    Работа над заданием 11 спринта, исправление для pull request

--- src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
@@ -13,7 +13,7 @@ import java.util.List;
 public class FilmController {
     private final FilmService filmService;
 
-    public FilmController(FilmService filmService){
+    public FilmController(FilmService filmService) {
         this.filmService = filmService;
     }
 

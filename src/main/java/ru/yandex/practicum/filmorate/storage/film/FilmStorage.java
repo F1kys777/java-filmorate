1bb7a7e45785212b7aea0e2dad2ddc23d88e500f
fikys <maximfeok2200@gmail.com>
2026-08-18 00:03:19 +0300

    Работа над заданием 11 спринта, исправление для pull request

--- src/main/java/ru/yandex/practicum/filmorate/storage/film/FilmStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/film/FilmStorage.java
@@ -7,9 +7,14 @@ import java.util.Collection;
 public interface FilmStorage {
 
     void addFilm(Film film);
+
     void updateFilm(long filmId, Film updatedFilm);
+
     void deleteFilm(long filmId);
+
     Film getFilmById(long filmId);
+
     Collection<Film> getAllFilms();
+
     boolean containsFilm(long filmId);
 }

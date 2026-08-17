47ae8a82833eb9b980f4b3b9af05d37a2a7c1d6f
fikys <maximfeok2200@gmail.com>
2026-08-17 23:58:14 +0300

    Работа над заданием 11 спринта, исправление для pull request

--- src/main/java/ru/yandex/practicum/filmorate/model/Film.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/Film.java
@@ -14,5 +14,5 @@ public class Film {
     private String description;
     private LocalDate releaseDate;
     private Integer duration;
-    private Set<Long> likes  = new HashSet<>();;
+    private Set<Long> likes = new HashSet<>();
 }

e62ed2f0b99b954ed42c37be7ff8e8ebc9da5e9f
Alexander-Golubev-int <a.golubevspb1608@gmail.com>
2026-09-20 20:12:19 +0300

    List

--- src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
@@ -9,6 +9,7 @@ import ru.yandex.practicum.filmorate.exception.ValidationException;
 import ru.yandex.practicum.filmorate.service.FilmService;
 
 import java.util.Collection;
+import java.util.List;
 
 @RestController
 @RequestMapping("/films")
